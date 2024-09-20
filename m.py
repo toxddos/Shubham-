b53ee46e7d9571cd2e5550c117a5b6fc46be41cf
toxddos <shubhamkal.u212@gmail.com>
2024-09-20 12:55:41 +0530

    Update m.py

--- m.py
+++ m.py
@@ -17,13 +17,13 @@ keep_alive()
 
 # insert your Telegram bot token here
 
-bot = telebot.TeleBot('7543592031:AAHnlEqSQg2MC_Dimz8Hi71iMD8GfYrweTM')
+bot = telebot.TeleBot('7274614073:AAGWhCLNcOqO0xoogJFyDZJCp6JXEnXpvPI')
 
 
 
 # Admin user IDs
 
-admin_id = ["6704542925"]
+admin_id = ["729917819"]
 
 
 
